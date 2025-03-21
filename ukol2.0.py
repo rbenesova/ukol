9954648a8d18abfb1cb4670a26287fde8419811e
rbenesova <renata.benesova@infis.cz>
2025-03-21 20:50:34 +0100

    Add files via upload

---
+++ ukol2.0.py
@@ -0,0 +1,12 @@
+cislo1 = float(input("zadejte první desetine cislo"))
+cislo2 = float(input("zadejte druhe desetine cislo"))
+
+soucet = cislo1+cislo2
+rozdil = cislo1-cislo2
+soucin = cislo1*cislo2
+podil = cislo1/cislo2
+
+print(f"soucet je {soucet:.2f}")
+print(f"rozdil je {rozdil:.2f}")
+print(f"soucin je {soucin:.2f}")
+print(f"podil je {podil:.2f}")
